8c6be1b6c7e6851a7759380221ba54032f8bd3b4
Alex <afb5309@psu.edu>
2019-06-14 22:32:55 -0400

    handlebars main page only

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var path = require("path");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -25,6 +26,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/layouts/partials")
+}));
+app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
@@ -66,6 +75,7 @@ console.log("I'm scraping okay!")
       db.Article.create(result)
         .then(function(dbArticle) {
           // View the added result in the console
+          res.render("index", {dbArticle: result})
           console.log(dbArticle);
         })
         .catch(function(err) {
@@ -75,8 +85,40 @@ console.log("I'm scraping okay!")
     });
 
     // Send a message to the client
+    console.log(result)
     res.send("Scrape Complete");
   });
+});
+
+app.get("/", function (req, res) {
+  db.Article.find({ "saved": false }, function (error, data) {
+      var hbsObject = {
+          article: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+  });
+});
+
+app.get("/saved", function (req, res) {
+  db.Article.find({ "saved": true }).populate("notes").exec(function (error, articles) {
+      var hbsObject = {
+          article: articles
+      };
+      res.render("saved", hbsObject);
+  });
+});
+
+app.get('/clear', function(req, res) {
+  db.Article.remove({ saved: false}, function(err, doc) {
+      if (err) {
+          console.log(err);
+      } else {
+          console.log('removed');
+      }
+
+  });
+  res.redirect('/');
 });
 
 // Route for getting all Articles from the db
@@ -102,6 +144,7 @@ app.get("/articles/:id", function(req, res) {
     .then(function(dbArticle) {
       // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
+      res.render("index", hbsObject)
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
@@ -110,23 +153,39 @@ app.get("/articles/:id", function(req, res) {
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+// app.post("/articles/:id", function(req, res) {
+//   // Create a new note and pass the req.body to the entry
+//   db.Note.create(req.body)
+//     .then(function(dbNote) {
+//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+//       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+//     })
+//     .then(function(dbArticle) {
+//       // If we were able to successfully update an Article, send it back to the client
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
+
+app.post("/articles/save/:id", function (req, res) {
+  // Use the article id to find and update its saved boolean
+  db.Article.findOneAndUpdate({ _id: req.params.id }, { "saved": true })
+      // Execute the above query
+      .exec(function (err, data) {
+          // Log any errors
+          if (err) {
+              console.log(err);
+          }
+          else {
+
+              res.send(data);
+          }
+      });
 });
 
 // Start the server
