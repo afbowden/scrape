4d435fbecf656ac03bc416c762453d2826b3a4e2
Alex <afb5309@psu.edu>
2019-06-15 20:36:15 -0400

    port env

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 // Initialize Express
 var app = express();
